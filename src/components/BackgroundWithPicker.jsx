8e45a7ca39a1d50cf439afced1d1bddbd21f1b3d
Abdullah Al MahMud <abdudevs@gmail.com>
2023-06-07 17:55:09 +0600

    added a fully working color picker and changer

--- src/components/BackgroundWithPicker.jsx
+++ src/components/BackgroundWithPicker.jsx
@@ -4,13 +4,22 @@ import TextDivider from "./TextDivider";
 
 const BackgroundWithPicker = () => {
   const { token } = theme.useToken();
-  const [color, setColor] = useState(token.colorPrimary);
-  const bgColor = useMemo(
-    () => (typeof color === "string" ? color : color.toHexString()),
-    [color]
+  const [textColor, setTextColor] = useState(token.colorPrimary);
+  const [bgColor, setBgColor] = useState("#ffffff");
+
+  const textColorStyle = useMemo(
+    () => (typeof textColor === "string" ? textColor : textColor.toHexString()),
+    [textColor]
   );
+
+  const bgColorStyle = useMemo(
+    () => (typeof bgColor === "string" ? bgColor : bgColor.toHexString()),
+    [bgColor]
+  );
+
   const componentStyle = {
-    color: bgColor,
+    color: textColorStyle,
+    backgroundColor: bgColorStyle,
   };
 
   return (
@@ -18,42 +27,89 @@ const BackgroundWithPicker = () => {
       style={componentStyle}
       className="flex flex-col gap-6 border-2 border-blue-400 rounded-md p-6"
     >
-      <ColorPicker
-        presets={[
-          {
-            label: "Recommended",
-            colors: [
-              "#000000",
-              "#000000A6",
-              "#00000073",
-              "#00000005",
-              "#F5222D",
-              "#FA8C16",
-              "#FADB14",
-              "#8BBB11",
-              "#52C41A",
-              "#13A8A8",
-              "#1677FF",
-              "#2F54EB",
-              "#722ED1",
-              "#EB2F96",
-              "#F5222D4D",
-              "#FA8C164D",
-              "#FADB144D",
-              "#8BBB114D",
-              "#52C41A4D",
-              "#EB2F964D",
-            ],
-          },
-          {
-            label: "Recent",
-            colors: [],
-          },
-        ]}
-        value={color}
-        onChange={setColor}
-      />
-      <TextDivider></TextDivider>
+      <section className="flex items-center justify-between gap-4">
+        <div>
+          <label htmlFor="text-color-picker">Text Color:</label>
+          <ColorPicker
+            id="text-color-picker"
+            presets={[
+              {
+                label: "Recommended",
+                colors: [
+                  "#000000",
+                  "#000000A6",
+                  "#00000073",
+                  "#00000005",
+                  "#F5222D",
+                  "#FA8C16",
+                  "#FADB14",
+                  "#8BBB11",
+                  "#52C41A",
+                  "#13A8A8",
+                  "#1677FF",
+                  "#2F54EB",
+                  "#722ED1",
+                  "#EB2F96",
+                  "#F5222D4D",
+                  "#FA8C164D",
+                  "#FADB144D",
+                  "#8BBB114D",
+                  "#52C41A4D",
+                  "#EB2F964D",
+                ],
+              },
+              {
+                label: "Recent",
+                colors: [],
+              },
+            ]}
+            value={textColor}
+            onChange={setTextColor}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="bg-color-picker">Background Color:</label>
+          <ColorPicker
+            id="bg-color-picker"
+            presets={[
+              {
+                label: "Recommended",
+                colors: [
+                  "#000000",
+                  "#000000A6",
+                  "#00000073",
+                  "#00000005",
+                  "#F5222D",
+                  "#FA8C16",
+                  "#FADB14",
+                  "#8BBB11",
+                  "#52C41A",
+                  "#13A8A8",
+                  "#1677FF",
+                  "#2F54EB",
+                  "#722ED1",
+                  "#EB2F96",
+                  "#F5222D4D",
+                  "#FA8C164D",
+                  "#FADB144D",
+                  "#8BBB114D",
+                  "#52C41A4D",
+                  "#EB2F964D",
+                ],
+              },
+              {
+                label: "Recent",
+                colors: [],
+              },
+            ]}
+            value={bgColor}
+            onChange={setBgColor}
+          />
+        </div>
+      </section>
+
+      <TextDivider />
     </div>
   );
 };
